0d4b173603cff3149f523ab3fc053a92e45f3c6c
grum1n <grumin.el.pastas@gmail.com>
2021-12-17 17:05:05 +0200

    added new components Header, Footer . Home page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
 import React from 'react';
 import './styles/frontStyle.css';
+import Header from './webFrontSide/components/Header';
+import AppRoutes from './AppRoutes';
+import Footer from './webFrontSide/components/Footer';
 
 const App = () => {
   return (
     <div>
-      test
+      <Header />
+      <main>
+        <AppRoutes/>
+      </main>
+      <Footer />
     </div>
   );
 };
