d535d6e08add070674ec97bd6d970528bc9ae4c1
grum1n <grumin.el.pastas@gmail.com>
2021-12-17 23:33:43 +0200

    to login component temporarily added link to dashboard page

--- src/webFrontSide/pages/Login.js
+++ src/webFrontSide/pages/Login.js
@@ -8,7 +8,8 @@ const Login = () => {
         <form className="flex-column form-box">
           <input className="login-input" type="text" placeholder="Email" />
           <input className="login-input" type="password" placeholder="Password"/>
-          <input className="registerButton" type="button" value=" Log in " />
+          {/* <input className="registerButton" type="button" value=" Log in " /> */}
+          <Link className="registerButton" to="/dashboard">Log in</Link>
         </form>
         <p>
           <Link className="login-link" to="/register">Link to register page</Link>
