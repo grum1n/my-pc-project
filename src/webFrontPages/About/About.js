36434a30e294deb68389a6766ed6b540d094e3cb
grum1n <grumin.el.pastas@gmail.com>
2021-12-21 17:29:10 +0200

    added content , inside about page

--- src/webFrontPages/About/About.js
+++ src/webFrontPages/About/About.js
@@ -1,22 +1,64 @@
 import React from 'react';
 import Header from '../../components/Header';
+import './about.css';
 
 const About = () => {
   return (
-        <>
-            <Header />
-                <main className='frontSide-main'>
-                    <section className='hero'>
-                    <h2>
-                        Apie mus
-                    </h2>
-                    </section>
-                    <div className='container'>
-                        Profilis
-                    </div>
-                </main>
-
-        </>
+    <>
+        <Header />
+        <main className='frontSide-main'>
+            <div className='hero'>
+            <h2>
+                Apie mus
+            </h2>
+            </div>
+            <div className='container about-grid'>
+                <div className='col-1'>img</div>
+                <div className='col-2'>
+                    <h3>Mūsų profilis</h3>
+                    <p className='about-bold-text'>textt ajdfsjkd</p>
+                    <p>text kanjancjksdn</p>
+                    <p>text knsjvnfvjn</p>
+                </div>
+                <div className='col-3'>
+                    <h3>Mūsų projekto pradžia</h3>
+                    <p>Projektas pradėtas kurti per Present Connection Academijos kursą</p>
+                    <div className='grid-images-box'>img</div>
+                </div>
+                <div className='col-4'>
+                    <div className='grid-images-box'>img</div>
+                    <h3>Rinka</h3>
+                    <p>Naudotų automobilių detalių prekyba internete yra auganti rinka.</p>
+                </div>
+                <div className='col-5'>
+                    <div className='design-color-box'></div>
+                    <div className='grid-images-box'>img</div>
+                    <h3>Tikslas</h3>
+                    <p>Sukurti patogią naudotų automobilių detalių sandėliavimo platformą</p>
+                </div>
+            </div>
+        </main>
+        <section className='stats-container'>
+            <ul className='container flex-around'>
+                <li>
+                    <h2>13000+</h2>
+                    <h5>Naudota paieška</h5>
+                </li>
+                <li>
+                    <h2>64+</h2>
+                    <h5>Laužynai</h5>
+                </li>
+                <li>
+                    <h2>550/d.</h2>
+                    <h5>Įkeliama detalių</h5>
+                </li>
+                <li>
+                    <h2>148.000</h2>
+                    <h5>Sandėlyje datalių</h5>
+                </li>
+            </ul>
+        </section>
+    </>
   );
 };
 
